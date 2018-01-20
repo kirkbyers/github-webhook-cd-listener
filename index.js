69838e0900615a163453a2947ba7b6657c464977
kirkbyers <kirklbyers@gmail.com>
2018-01-19 22:12:21 -0600

    Fix pathing bug when using tool from bin

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var express = require("express");
 var path_1 = require("path");
 var app = express();
 program
-    .version('0.0.0')
+    .version('0.1.0')
     .option('-p, --port <n>', 'Port number to listen to', parseInt)
     .option('-e, --exec <s>', 'Relative path to script to execute on webhook POST')
     .option('-s, --secret <s>', 'Webhook secret to compare to on POST')
     .parse(process.argv);
 var portNumber = program.port || 6767;
-var scriptInputPath = program.script || './webhook.sh';
-var scriptPath = path_1.resolve(__dirname, scriptInputPath);
+var scriptInputPath = program.exec || './webhook.sh';
+var scriptPath = path_1.resolve(process.cwd(), scriptInputPath);
 console.log("Starting Webhook listener on port " + portNumber);
 app.post('/', function (req, res) {
     child_process_1.exec("sh " + scriptPath);
--- index.ts
+++ index.ts
@@ -6,15 +6,15 @@ import { resolve } from 'path';
 const app = express();
 
 program
-  .version('0.0.0')
+  .version('0.1.0')
   .option('-p, --port <n>', 'Port number to listen to', parseInt)
   .option('-e, --exec <s>', 'Relative path to script to execute on webhook POST')
   .option('-s, --secret <s>', 'Webhook secret to compare to on POST')
   .parse(process.argv);
 
 const portNumber = program.port || 6767;
-const scriptInputPath = program.script || './webhook.sh';
-const scriptPath = resolve(__dirname, scriptInputPath);
+const scriptInputPath = program.exec || './webhook.sh';
+const scriptPath = resolve(process.cwd(), scriptInputPath);
 
 console.log(`Starting Webhook listener on port ${portNumber}`);
 
